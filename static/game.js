e7709346cd08caafb0d21623c7c8b15fe53e78cf
MorotAmaury <amaury.morot39@gmail.com>
2024-03-26 11:16:33 +0100

    ajout le misisle

--- static/game.js
+++ static/game.js
@@ -1,17 +1,11 @@
 let player = document.getElementById('player');
-<<<<<<< HEAD
-let wall = document.getElementById('wall');
-let leftPosition = 900;
-let topPosition = 500;
-let speed = 2;
-=======
 
 // partie sur le mouvement
 
+let wall = document.getElementById('wall');
 let xPos = 900; // position horizontal de depart
 let yPos = 500; // position vertical de depart
 let speed = 2; // vitesse du joueur
->>>>>>> 3a7693534ca00cec48dd7124f4032c914fa8ddb1
 let keyState = {};
 
 let isCollide = (obstacle, player) => {
@@ -22,17 +16,6 @@ let isCollide = (obstacle, player) => {
 
 
 let movePlayer = () => {
-<<<<<<< HEAD
-
-    player.style.left = leftPosition + 'px';
-    player.style.top = topPosition + 'px';
-};
-
-let updatePosition = () => {
-
-    if (keyState['ArrowLeft'] && leftPosition - speed > 100) {
-        leftPosition -= speed;
-=======
     player.style.left = xPos + 'px';
     player.style.top = yPos + 'px';
 };
@@ -40,7 +23,6 @@ let updatePosition = () => {
 let updatePosition = () => {
     if (keyState['ArrowLeft'] && xPos - speed > 0) { // aller a gauche
         xPos -= speed;
->>>>>>> 3a7693534ca00cec48dd7124f4032c914fa8ddb1
     }
     if (keyState['ArrowRight'] && xPos + speed < (window.innerWidth - 50)) { // aller a droite
         xPos += speed;
@@ -70,9 +52,6 @@ setInterval(() => {
         updatePosition();
     }
     movePlayer();
-<<<<<<< HEAD
-}, 1); // Environ 60 images par seconde
-=======
 }, 1); // Environ 60 images par seconde
 
 // partie tirer
@@ -80,36 +59,33 @@ setInterval(() => {
 
 
 document.addEventListener('click', (e) => {
-    const missile = document.createElement('div')
-    missile.classList.add('test')
-    missile.style.left = player.style.left
-    missile.style.top = player.style.top
-
-    document.getElementById('missile-container').appendChild(missile)
-
-    let missile_mouvement = (incrementationX, incrementationY) => {
-        return new Promise(resolve => {
-            while (parseInt(missile.style.left) < window.innerWidth + 500 && parseInt(missile.style.left) > -500 && 
-            parseInt(missile.style.top) < window.innerHeight + 500 && parseInt(missile.style.top) > -500) 
-            {
-                console.log(parseInt(missile.style.left),  incrementationX);
-                missile.style.left = (parseInt(missile.style.left) + incrementationX) + 'px'
-                missile.style.top = (parseInt(missile.style.top) + incrementationY) + 'px'
+    const missile = document.createElement('div');
+    missile.classList.add('missile');
     
-            }
-            resolve()
-        })
+    let player = document.getElementById('player');
+    let posY = parseInt(player.style.top) - 0.8 * (window.innerWidth / 100);
+    let posX = parseInt(player.style.left) + 1.6 * (window.innerWidth / 100);
+    missile.style.left = posX + "px";
+    missile.style.top = posY + "px";
+    var testx = `${(e.clientX - posX)*10}px`
+    var testy = `${(e.clientY - posY)*10}px`
+    
+    if (parseInt(testx) < 500 && parseInt(testx) > -500 || parseInt(testy) < 100 && parseInt(testy) > -100)
+    {
+        missile.classList.add('test')
     }
-
-    setTimeout(async() => {
-        let incrementationX = e.clientX - Math.abs(parseInt(missile.style.left))
-        let incrementationY = e.clientY - Math.abs(parseInt(missile.style.top))
-        await missile_mouvement(incrementationX, incrementationY)
-    }, 30)
-
     
+    document.getElementById('missile-container').appendChild(missile);
+    
+    
+    setTimeout(async () => {
+        console.log(testx, testy);
+        missile.style.transform = `translate(${testx}, ${testy})`;
+            
         
+    }, 30);
+});
 
-})
 
->>>>>>> 3a7693534ca00cec48dd7124f4032c914fa8ddb1
+
+
